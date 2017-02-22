d250a6bf260165e54b0d75dde69bf4b6b45df055
Håkon Ødegård Løvdal <me@hakloev.no>
2017-02-22 22:11:14 +0100

    Change article api route to only accept get requests

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,9 +4,10 @@ from .serializers import ArticleSerializer
 # Create your views here.
 
 
-class ArticleViewSet(viewsets.ModelViewSet):
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allow users to view or edit articles
+    API endpoint that allow users to view all articles
+    ReadOnly in order to ensure no unauthorized editing
     """
     queryset = Article.objects.published()
     serializer_class = ArticleSerializer
